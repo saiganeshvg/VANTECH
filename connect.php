c797341f8af1019616e2dc3704f44bc235b768bd
saiganeshvg <saivanguri42@gmail.com>
2026-03-07 15:49:29 +0530

    added name to each section of contact box refer to sql

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $name  =  filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-$email  =  filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+$email  =  filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $subject  =  filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
 $message  =  filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
 
@@ -12,14 +12,15 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 if ($conn->connect_error) {
     die('Connect Error ('. $conn->connect_errno .') '.$conn->connect_error);
 } else {
-    $stmt = $conn->prepare("INSERT INTO account (name, email, subject, message) VALUES (?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO user (name, email, subject, message) VALUES (?, ?, ?, ?)");
     if ($stmt === false) {
         die('Prepare failed: ' . $conn->error);
     }
     $stmt->bind_param("ssss", $name, $email, $subject, $message);
   if ($stmt->execute()) {
-    echo "You sent message";
-  } else {
+    echo 'Your message has been sent';
+  } 
+  else {
     echo "Error; " . $stmt->error;
   }
   $stmt->close();
